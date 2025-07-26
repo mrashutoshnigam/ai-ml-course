88cce05ca6af41905c723dd7ff93fc022288ee0f
ashutosh nigam <8054735+mrashutoshnigam@users.noreply.github.com>
2025-07-26 22:07:12 +0530

    Refactor autoencoder classes and training function for improved clarity; update interpolation analysis and metrics reporting

--- DeepLearning_GeeksForGeeks/Programming_Assignment/final_python_code.py
+++ DeepLearning_GeeksForGeeks/Programming_Assignment/final_python_code.py
@@ -1,6 +1,5 @@
 import tensorflow as tf
 from tensorflow.keras import layers, models
-from tensorflow.keras.datasets import mnist
 import seaborn as sns
 from sklearn.metrics import mean_squared_error
 from sklearn.linear_model import LogisticRegression
@@ -53,16 +52,14 @@ test_dataset = tf.data.Dataset.from_tensor_slices(
 # U-Net-like Encoder and decoder for MNIST
 def build_encoder():
     inputs = layers.Input(shape=(28, 28, 1))
-    x = layers.Conv2D(64, 3, padding='same',
-                      activation='relu')(inputs)  # 28x28x64
-    x = layers.MaxPooling2D(2)(x)  # 14x14x64
-    x = layers.Conv2D(128, 3, padding='same',
-                      activation='relu')(x)  # 14x14x128
-    x = layers.MaxPooling2D(2)(x)  # 7x7x128
-    x = layers.Conv2D(256, 3, padding='same', activation='relu')(x)  # 7x7x256
-    x = layers.MaxPooling2D(2)(x)  # 3x3x256
+    x = layers.Conv2D(64, 3, padding='same', activation='relu')(inputs)
+    x = layers.MaxPooling2D(2)(x)
+    x = layers.Conv2D(128, 3, padding='same', activation='relu')(x)
+    x = layers.MaxPooling2D(2)(x)
+    x = layers.Conv2D(256, 3, padding='same', activation='relu')(x)
+    x = layers.MaxPooling2D(2)(x)
     x = layers.Flatten()(x)
-    z = layers.Dense(128)(x)  # Latent space
+    z = layers.Dense(128)(x)
     return models.Model(inputs, z, name='encoder')
 
 
@@ -71,14 +68,14 @@ def build_decoder():
     x = layers.Dense(256 * 3 * 3, activation='relu')(inputs)
     x = layers.Reshape((3, 3, 256))(x)
     x = layers.Conv2DTranspose(
-        128, 3, strides=2, padding='same', activation='relu')(x)  # 6x6x128
+        128, 3, strides=2, padding='same', activation='relu')(x)
     x = layers.Conv2DTranspose(
-        64, 3, strides=2, padding='same', activation='relu')(x)  # 12x12x64
+        64, 3, strides=2, padding='same', activation='relu')(x)
     x = layers.Conv2DTranspose(
-        1, 3, strides=2, padding='same', activation='sigmoid')(x)  # 28x28x1
+        1, 3, strides=2, padding='same', activation='sigmoid')(x)
     x = layers.Conv2D(1, 3, padding='valid',
-                      activation='sigmoid')(x)  # 22x22x1
-    x = layers.ZeroPadding2D(padding=((3, 3), (3, 3)))(x)  # 28x28x1
+                      activation='sigmoid')(x)
+    x = layers.ZeroPadding2D(padding=((3, 3), (3, 3)))(x)
     return models.Model(inputs, x, name='decoder')
 
 
@@ -86,66 +83,73 @@ def build_decoder():
 class SparseAutoencoder(models.Model):
     def __init__(self):
         super(SparseAutoencoder, self).__init__()
-        self.encoder = build_encoder()
-        self.decoder = build_decoder()
+        self.encoder_network = build_encoder()
+        self.decoder_network = build_decoder()
 
-    def call(self, inputs):
-        z = self.encoder(inputs)
-        recon = self.decoder(z)
-        return recon, z
+    def call(self, input_images):
+        encoded_features = self.encoder_network(input_images)
+        reconstructed_images = self.decoder_network(encoded_features)
+        return reconstructed_images, encoded_features
 
-    def loss(self, y_true, y_pred, z):
-        mse_loss = tf.reduce_mean(tf.keras.losses.mse(y_true, y_pred))
-        rho_hat = tf.reduce_mean(z, axis=0)
-        rho_hat = tf.clip_by_value(rho_hat, epsilon, 1 - epsilon)
-        kl_div = rho * tf.math.log(rho / rho_hat) + \
-            (1 - rho) * tf.math.log((1 - rho) / (1 - rho_hat))
-        # Clip KL term
-        kl_loss = sparse_lambda * tf.reduce_sum(
-            tf.clip_by_value(kl_div, -1e4, 1e4))
-        return mse_loss + kl_loss
+    def loss(self, original_images, reconstructed_images, encoded_features):
+        reconstruction_loss = tf.reduce_mean(
+            tf.keras.losses.mse(original_images, reconstructed_images))
+        average_activation = tf.reduce_mean(encoded_features, axis=0)
+        clipped_activation = tf.clip_by_value(
+            average_activation, epsilon, 1 - epsilon)
+        divergence = rho * tf.math.log(rho / clipped_activation) + \
+            (1 - rho) * tf.math.log((1 - rho) / clipped_activation)
+        sparsity_penalty = sparse_lambda * tf.reduce_sum(
+            tf.clip_by_value(divergence, -1e4, 1e4))
+        return reconstruction_loss + sparsity_penalty
 
 
 class ContractiveAutoencoder(models.Model):
     def __init__(self):
         super(ContractiveAutoencoder, self).__init__()
-        self.encoder = build_encoder()
-        self.decoder = build_decoder()
+        self.encoder_network = build_encoder()
+        self.decoder_network = build_decoder()
 
-    def call(self, inputs):
-        z = self.encoder(inputs)
-        recon = self.decoder(z)
-        return recon, z
+    def call(self, input_images):
+        encoded_features = self.encoder_network(input_images)
+        reconstructed_images = self.decoder_network(encoded_features)
+        return reconstructed_images, encoded_features
 
-    def loss(self, x, recon, z, model):
-        mse_loss = tf.reduce_mean(tf.square(x - recon))
+    def loss(self, original_images, reconstructed_images, encoded_features, model):
+        reconstruction_loss = tf.reduce_mean(
+            tf.square(original_images - reconstructed_images))
         with tf.GradientTape() as tape:
-            tape.watch(z)
-            recon = model.decoder(z)
-        grad_z = tape.gradient(recon, z)
-        j_loss = contractive_lambda * tf.reduce_mean(
-            tf.reduce_sum(tf.square(grad_z), axis=1))
-        return mse_loss + j_loss
+            tape.watch(encoded_features)
+            reconstructed_output = model.decoder_network(encoded_features)
+        jacobian_gradients = tape.gradient(
+            reconstructed_output, encoded_features)
+        contractive_penalty = contractive_lambda * tf.reduce_mean(
+            tf.reduce_sum(tf.square(jacobian_gradients), axis=1))
+        return reconstruction_loss + contractive_penalty
 
 
 # Train function for both Sparse and Contractive Autoencoder
-def train(model, dataset, loss_fn, epochs, model_type='sparse'):
+def train(autoencoder_model, training_dataset, loss_function, num_epochs, autoencoder_type='sparse'):
     optimizer = tf.keras.optimizers.Adam(learning_rate)
-    for epoch in range(epochs):
-        total_loss = 0
-        for batch in dataset:
-            with tf.GradientTape() as tape:
-                recon, z = model(batch)
-                if model_type == 'sparse':
-                    loss = loss_fn(batch, recon, z)
+    for current_epoch in range(num_epochs):
+        epoch_total_loss = 0
+        for training_batch in training_dataset:
+            with tf.GradientTape() as gradient_tape:
+                reconstructed_images, encoded_features = autoencoder_model(
+                    training_batch)
+                if autoencoder_type == 'sparse':
+                    current_loss = loss_function(
+                        training_batch, reconstructed_images, encoded_features)
                 else:
-                    loss = loss_fn(batch, recon, z, model)
-            gradients = tape.gradient(loss, model.trainable_variables)
+                    current_loss = loss_function(
+                        training_batch, reconstructed_images, encoded_features, autoencoder_model)
+            computed_gradients = gradient_tape.gradient(
+                current_loss, autoencoder_model.trainable_variables)
             optimizer.apply_gradients(
-                zip(gradients, model.trainable_variables))
-            total_loss += loss.numpy()
+                zip(computed_gradients, autoencoder_model.trainable_variables))
+            epoch_total_loss += current_loss.numpy()
         print(
-            f'Epoch {epoch+1}/{epochs}, {model_type.capitalize()} AE Loss: {total_loss / len(dataset): .6f}')
+            f'Epoch {current_epoch+1}/{num_epochs}, {autoencoder_type.capitalize()} AE Loss: {epoch_total_loss / len(training_dataset): .6f}')
 
 
 def reconstruct_and_validate(model, dataset, model_name, num_images=5):
@@ -174,6 +178,7 @@ def reconstruct_and_validate(model, dataset, model_name, num_images=5):
     plt.tight_layout()
     plt.savefig(f"./data/output/{model_name}_reconstruction.png")
     # plt.show()
+    plt.close()
 
 # Function to plot t-SNE of embeddings
 
@@ -204,6 +209,7 @@ def plot_tsne_embeddings(model, dataset, model_name, num_samples=1000):
     plt.tight_layout()
     plt.savefig(f"./data/output/{model_name}_tsne.png")
     # plt.show()
+    plt.close()
 
 
 def compute_psnr(img1, img2, max_val=1.0):
@@ -216,121 +222,119 @@ def compute_psnr(img1, img2, max_val=1.0):
 
 
 def interpolation_analysis(model, dataset, model_name, num_pairs=20, num_images_per_pair=5):
-    # Collect images and labels from test set
-    all_images, all_labels = [], []
-    for images, labels in dataset:
-        all_images.append(images.numpy())
-        all_labels.append(labels.numpy())
-    all_images = np.concatenate(all_images, axis=0)
-    all_labels = np.concatenate(all_labels, axis=0)
+    all_test_images, all_test_labels = [], []
+    for batch_images, batch_labels in dataset:
+        all_test_images.append(batch_images.numpy())
+        all_test_labels.append(batch_labels.numpy())
+    all_test_images = np.concatenate(all_test_images, axis=0)
+    all_test_labels = np.concatenate(all_test_labels, axis=0)
 
-    # Randomly select pairs from different classes
-    np.random.seed(42)  # For reproducibility
-    indices = np.arange(len(all_labels))
-    np.random.shuffle(indices)
-    selected_pairs = []
-    used_labels = set()
-    i = 0
-    while len(selected_pairs) < num_pairs and i < len(indices):
-        idx1 = indices[i]
-        label1 = all_labels[idx1]
-        # Find an index with a different label
-        for j in range(i + 1, len(indices)):
-            idx2 = indices[j]
-            label2 = all_labels[idx2]
-            if label1 != label2 and (label1, label2) not in used_labels:
-                selected_pairs.append((idx1, idx2))
-                used_labels.add((label1, label2))
+    np.random.seed(42)
+    image_indices = np.arange(len(all_test_labels))
+    np.random.shuffle(image_indices)
+    selected_image_pairs = []
+    used_label_pairs = set()
+    current_index = 0
+
+    while len(selected_image_pairs) < num_pairs and current_index < len(image_indices):
+        first_image_idx = image_indices[current_index]
+        first_label = all_test_labels[first_image_idx]
+
+        for next_index in range(current_index + 1, len(image_indices)):
+            second_image_idx = image_indices[next_index]
+            second_label = all_test_labels[second_image_idx]
+
+            if first_label != second_label and (first_label, second_label) not in used_label_pairs:
+                selected_image_pairs.append(
+                    (first_image_idx, second_image_idx))
+                used_label_pairs.add((first_label, second_label))
                 break
-        i += 1
+        current_index += 1
 
-    # Alpha values for interpolation
-    alphas = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0]
+    interpolation_alphas = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0]
+    psnr_metrics = {alpha: [] for alpha in interpolation_alphas}
+    l2_norm_metrics = {alpha: [] for alpha in interpolation_alphas}
 
-    # Store metrics
-    psnr_values = {alpha: [] for alpha in alphas}
-    l2_norms = {alpha: [] for alpha in alphas}
+    for pair_index, (first_idx, second_idx) in enumerate(selected_image_pairs[:num_pairs]):
+        first_image = all_test_images[first_idx:first_idx+1]
+        second_image = all_test_images[second_idx:second_idx+1]
+        first_digit_label, second_digit_label = all_test_labels[
+            first_idx], all_test_labels[second_idx]
 
-    # Process each pair
-    for pair_idx, (idx1, idx2) in enumerate(selected_pairs[:num_pairs]):
-        I1 = all_images[idx1:idx1+1]  # Shape: (1, 28, 28, 1)
-        I2 = all_images[idx2:idx2+1]
-        label1, label2 = all_labels[idx1], all_labels[idx2]
+        first_embedding = model.encoder(first_image, training=False).numpy()
+        second_embedding = model.encoder(second_image, training=False).numpy()
 
-        # Get embeddings h1 and h2
-        h1 = model.encoder(I1, training=False).numpy()  # Shape: (1, 64)
-        h2 = model.encoder(I2, training=False).numpy()
+        plt.figure(figsize=(12, len(interpolation_alphas) * 2))
 
-        # Plot for this pair
-        plt.figure(figsize=(12, len(alphas) * 2))
-        for i, alpha in enumerate(alphas):
-            # Compute interpolated image Iα
-            I_alpha = alpha * I1 + (1 - alpha) * I2
-            # Compute embedding hα = E(Iα)
-            h_alpha = model.encoder(I_alpha, training=False).numpy()
-            # Compute approximate embedding h′α = αh1 + (1−α)h2
-            h_prime_alpha = alpha * h1 + (1 - alpha) * h2
-            # Decode to get Îα and Î′α
-            I_hat_alpha = model.decoder(h_alpha, training=False).numpy()
-            I_hat_prime_alpha = model.decoder(
-                h_prime_alpha, training=False).numpy()
+        for alpha_index, alpha_weight in enumerate(interpolation_alphas):
+            interpolated_image = alpha_weight * \
+                first_image + (1 - alpha_weight) * second_image
 
-            # Compute metrics
-            psnr = compute_psnr(I_hat_alpha[0], I_hat_prime_alpha[0])
-            l2_norm = np.sqrt(np.sum((h_alpha - h_prime_alpha) ** 2))
-            psnr_values[alpha].append(psnr)
-            l2_norms[alpha].append(l2_norm)
+            interpolated_embedding = model.encoder(
+                interpolated_image, training=False).numpy()
 
-            # Plot Îα and Î′α
-            plt.subplot(len(alphas), 2, i * 2 + 1)
-            plt.imshow(I_hat_alpha[0].reshape(28, 28), cmap='gray')
-            plt.title(f"Îα (α={alpha:.1f})")
+            interpolated_embedding_direct = alpha_weight * \
+                first_embedding + (1 - alpha_weight) * second_embedding
+
+            reconstructed_from_interpolated = model.decoder(
+                interpolated_embedding, training=False).numpy()
+            reconstructed_from_direct = model.decoder(
+                interpolated_embedding_direct, training=False).numpy()
+
+            psnr_value = compute_psnr(
+                reconstructed_from_interpolated[0], reconstructed_from_direct[0])
+            l2_distance = np.sqrt(
+                np.sum((interpolated_embedding - interpolated_embedding_direct) ** 2))
+
+            psnr_metrics[alpha_weight].append(psnr_value)
+            l2_norm_metrics[alpha_weight].append(l2_distance)
+
+            plt.subplot(len(interpolation_alphas), 2, alpha_index * 2 + 1)
+            plt.imshow(reconstructed_from_interpolated[0].reshape(
+                28, 28), cmap='gray')
+            plt.title(f"Îα (α={alpha_weight:.1f})")
             plt.axis('off')
-            plt.subplot(len(alphas), 2, i * 2 + 2)
-            plt.imshow(I_hat_prime_alpha[0].reshape(28, 28), cmap='gray')
-            plt.title(f"Î′α (α={alpha:.1f})")
+
+            plt.subplot(len(interpolation_alphas), 2, alpha_index * 2 + 2)
+            plt.imshow(reconstructed_from_direct[0].reshape(
+                28, 28), cmap='gray')
+            plt.title(f"Î′α (α={alpha_weight:.1f})")
             plt.axis('off')
 
         plt.suptitle(
-            f"{model_name} Pair {pair_idx+1}: Digit {label1} to {label2}")
+            f"{model_name} Pair {pair_index+1}: Digit {first_digit_label} to {second_digit_label}")
         plt.tight_layout()
-        plt.savefig(f"./data/output/{model_name}_pair_{pair_idx+1}.png")
-        # plt.show()
+        plt.savefig(f"./data/output/{model_name}_pair_{pair_index+1}.png")
+        plt.close()
 
-    # Report average metrics
+    # Print summary metrics
     print(f"\n{model_name} Metrics:")
     print("Alpha | Avg PSNR (dB) | Avg L2 Norm")
     print("------|---------------|------------")
-    for alpha in alphas:
-        avg_psnr = np.mean(psnr_values[alpha])
-        avg_l2_norm = np.mean(l2_norms[alpha])
-        print(f"{alpha:.1f}  | {avg_psnr:.4f}      | {avg_l2_norm:.4f}")
+    for alpha_weight in interpolation_alphas:
+        average_psnr = np.mean(psnr_metrics[alpha_weight])
+        average_l2_norm = np.mean(l2_norm_metrics[alpha_weight])
+        print(f"{alpha_weight:.1f}  | {average_psnr:.4f}      | {average_l2_norm:.4f}")
 
 
-# Function to perform classification and evaluate embeddings
 def classify_embeddings(model, train_data, train_labels, test_data, test_labels, model_name):
-    # Extract embeddings for training set
     train_embeddings = []
     for batch in tf.data.Dataset.from_tensor_slices(train_data).batch(batch_size):
         z = model.encoder(batch, training=False).numpy()
         train_embeddings.append(z)
     train_embeddings = np.concatenate(train_embeddings, axis=0)
 
-    # Extract embeddings for test set
     test_embeddings = []
     for batch, _ in test_dataset:
         z = model.encoder(batch, training=False).numpy()
         test_embeddings.append(z)
     test_embeddings = np.concatenate(test_embeddings, axis=0)
 
-    # Train logistic regression classifier
     classifier = LogisticRegression(max_iter=1000, random_state=42)
     classifier.fit(train_embeddings, train_labels)
 
-    # Predict on test embeddings
     test_predictions = classifier.predict(test_embeddings)
 
-    # Compute accuracy
     accuracy = accuracy_score(test_labels, test_predictions)
     print(f"{model_name} Classification Accuracy: {accuracy:.4f}")
 
@@ -345,12 +349,10 @@ sparse_ae.summary()
 print("\nContractive Autoencoder Summary:")
 contractive_ae.summary()
 
-# Train Sparse Autoencoder
 print("Training Sparse Autoencoder...")
 train(sparse_ae, train_dataset, sparse_ae.loss,
       epochs, model_type='sparse')
 
-# Train Contractive Autoencoder
 print("\nTraining Contractive Autoencoder...")
 train(contractive_ae, train_dataset, contractive_ae.loss,
       epochs, model_type='contractive')
@@ -362,22 +364,18 @@ print("\nReconstructing and Validating Contractive Autoencoder...")
 reconstruct_and_validate(contractive_ae, test_dataset,
                          "Contractive Autoencoder")
 
-# After training and validation, plot t-SNE
 print("\nPlotting t-SNE for Sparse Autoencoder...")
 plot_tsne_embeddings(sparse_ae, test_dataset, "Sparse Autoencoder")
 
 print("\nPlotting t-SNE for Contractive Autoencoder...")
 plot_tsne_embeddings(contractive_ae, test_dataset, "Contractive Autoencoder")
 
-# After training and other validations, perform interpolation analysis
 print("\nInterpolation Analysis for Sparse Autoencoder...")
 interpolation_analysis(sparse_ae, test_dataset, "Sparse Autoencoder")
 
 print("\nInterpolation Analysis for Contractive Autoencoder...")
 interpolation_analysis(contractive_ae, test_dataset, "Contractive Autoencoder")
 
-
-# After training, reconstruction, t-SNE, and interpolation analysis
 print("\nClassifying Digits using Sparse Autoencoder Embeddings...")
 sparse_accuracy = classify_embeddings(
     sparse_ae, x_train, y_train, x_test, y_test, "Sparse Autoencoder")
@@ -386,7 +384,6 @@ print("\nClassifying Digits using Contractive Autoencoder Embeddings...")
 contractive_accuracy = classify_embeddings(
     contractive_ae, x_train, y_train, x_test, y_test, "Contractive Autoencoder")
 
-# Compare and report which is better
 print("\nComparison:")
 if sparse_accuracy > contractive_accuracy:
     print(
